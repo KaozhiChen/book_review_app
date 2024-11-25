ab161b88fb5c7b37567e93b9aab097cf89067612
KaozhiChen <kylerchen16@gmail.com>
2024-11-24 21:18:47 -0500

    add theme colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Primary colors
+const Color primaryColor = Color(0xFFF4A261); // Main theme color
+const Color secondaryColor =
+    Color(0xFFE76F51); // Accent for buttons and highlights
+const Color tertiaryColor = Color(0xFFF7C59F); // Background or card elements
+const Color forthColor =
+    Color(0xFF264653); // Text or icons on light backgrounds
+
+// Accent colors
+const Color highlightColor = Color(0xFFFFB703); // Highlights or warnings
+const Color lightGray = Color(0xFFF0F0F0); // General background
+
+// Text colors
+const Color textPrimary = Color(0xFF333333); // Primary text
+const Color textSecondary = Color(0xFF666666); // Secondary text or descriptions
+const Color textHighlight =
+    Color(0xFFFFFFFF); // Text on dark or vibrant backgrounds
